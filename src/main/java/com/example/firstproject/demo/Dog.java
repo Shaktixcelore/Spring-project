6d2cef6a9a948f89a450225e3a99d5bb8674660a
Shaktixcelore <shakti.singh@xcelore.com>
2024-08-30 17:50:33 +0530

    mapping concept

---
+++ src/main/java/com/example/firstproject/demo/Dog.java
@@ -0,0 +1,11 @@
+package com.example.firstproject.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog {
+
+ public String fun(){
+    return "try to learn";
+ }   
+}
--- src/main/java/com/example/firstproject/demo/MyClass.java
+++ src/main/java/com/example/firstproject/demo/MyClass.java
@@ -1,13 +1,17 @@
 package com.example.firstproject.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyClass {
 
-    @GetMapping("demo")
-    public String sayHello(){
-        return "hello world";
+    @Autowired
+    private Dog dog;
+
+    @GetMapping("ok")
+    public String ok() {
+        return dog.fun();
     }
 }
